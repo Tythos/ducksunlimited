9567cf7380a1a6c0177d443b3bdd87eeba23c1e5
Tythos <bekirkpatrick@proton.me>
2025-01-29 21:26:30 -0800

    minecraft has edible cheeseburgers now!

--- src/main/java/net/tythos/ducksunlimited/ModItems.java
+++ src/main/java/net/tythos/ducksunlimited/ModItems.java
@@ -8,6 +8,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.component.type.FoodComponent;
 
 public class ModItems {
     public static Item register(Item item, RegistryKey<Item> registryKey) {
@@ -15,15 +16,30 @@ public class ModItems {
         return registeredItem;
     }
 
+    // declare custom item keys
+
     public static final RegistryKey<Item> SUSPICIOUS_SUBSTANCE_KEY = RegistryKey.of(RegistryKeys.ITEM,
             Identifier.of("ducksunlimited", "suspicious_substance"));
+
+    public static final RegistryKey<Item> TEH_NOMZ_KEY = RegistryKey.of(RegistryKeys.ITEM,
+            Identifier.of("ducksunlimited", "teh_nomz"));
+
+    // register custom item definitions
 
     public static final Item SUSPICIOUS_SUBSTANCE = register(
             new Item(new Item.Settings().registryKey(SUSPICIOUS_SUBSTANCE_KEY)),
             SUSPICIOUS_SUBSTANCE_KEY);
 
+    public static final Item TEH_NOMZ = register(
+            new Item(new Item.Settings().registryKey(TEH_NOMZ_KEY).food(new FoodComponent.Builder().build())),
+            TEH_NOMZ_KEY);
+
+    // initialize mod with entries
+
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
                 .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.TEH_NOMZ));
     }
 }
