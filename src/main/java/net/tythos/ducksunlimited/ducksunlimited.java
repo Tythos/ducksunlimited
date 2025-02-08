d8d9d6bdf03b43b7a344d291436bb12a8ba2dbeb
Tythos <bekirkpatrick@proton.me>
2025-02-08 13:53:21 -0800

    added custom potion

--- src/main/java/net/tythos/ducksunlimited/ducksunlimited.java
+++ src/main/java/net/tythos/ducksunlimited/ducksunlimited.java
@@ -3,6 +3,15 @@ package net.tythos.ducksunlimited;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.util.Identifier;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
+import net.minecraft.item.Items;
 
 public class ducksunlimited implements ModInitializer {
 	public static final String MOD_ID = "ducksunlimited";
@@ -12,6 +21,10 @@ public class ducksunlimited implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Potion TATER_POTION = Registry.register(Registries.POTION,
+			Identifier.of("ducksunlimited", "tater"),
+			new Potion("tater", new StatusEffectInstance(StatusEffects.SPEED, 3600, 0)));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -19,5 +32,11 @@ public class ducksunlimited implements ModInitializer {
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
 		ModItems.initialize();
+		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(
+					Potions.WATER,
+					Items.POTATO,
+					Registries.POTION.getEntry(TATER_POTION));
+		});
 	}
 }
