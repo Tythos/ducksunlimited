1b57a67bced4d37424866cac423fd134fcc8cb4e
Tythos <bekirkpatrick@proton.me>
2025-02-10 22:37:01 -0800

    added custom renderer

--- src/client/java/net/tythos/ducksunlimited/ducksunlimitedClient.java
+++ src/client/java/net/tythos/ducksunlimited/ducksunlimitedClient.java
@@ -1,11 +1,13 @@
 package net.tythos.ducksunlimited;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class ducksunlimitedClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as
-		// rendering.
+		BlockEntityRendererFactories.register(
+				ModBlockEntities.COUNTER_BLOCK_ENTITY,
+				CounterBlockEntityRenderer::new);
 	}
 }
