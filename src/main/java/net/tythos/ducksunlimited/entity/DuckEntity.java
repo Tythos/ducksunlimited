2276726fd5c4e3a26ec96aebe98993a02cc8b388
Tythos <bekirkpatrick@proton.me>
2025-04-09 23:25:04 -0700

    WE DID IT!

--- src/main/java/net/tythos/ducksunlimited/entity/DuckEntity.java
+++ src/main/java/net/tythos/ducksunlimited/entity/DuckEntity.java
@@ -3,7 +3,6 @@ package net.tythos.ducksunlimited.entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
@@ -11,15 +10,13 @@ import net.minecraft.world.World;
 import net.minecraft.entity.damage.DamageSource;
 
 public class DuckEntity extends ChickenEntity {
-
     public DuckEntity(EntityType<? extends ChickenEntity> entityType, World world) {
         super(entityType, world);
     }
 
     public static DefaultAttributeContainer.Builder createDuckAttributes() {
-        return AnimalEntity.createMobAttributes()
-                .add(EntityAttributes.MAX_HEALTH, 4.0D)
-                .add(EntityAttributes.MOVEMENT_SPEED, 0.25D);
+        return ChickenEntity.createChickenAttributes()
+                .add(EntityAttributes.MAX_HEALTH, 4.0D);
     }
 
     @Override
