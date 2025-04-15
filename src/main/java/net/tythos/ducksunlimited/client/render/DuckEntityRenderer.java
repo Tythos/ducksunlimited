d361a9f9967e98f1c0ad759d992c93ed187b06f8
Tythos <bekirkpatrick@proton.me>
2025-04-14 22:16:53 -0700

    crashes

--- src/main/java/net/tythos/ducksunlimited/client/render/DuckEntityRenderer.java
+++ src/main/java/net/tythos/ducksunlimited/client/render/DuckEntityRenderer.java
@@ -3,7 +3,6 @@ package net.tythos.ducksunlimited.client.render;
 import net.tythos.ducksunlimited.entity.DuckEntity;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.state.ChickenEntityRenderState;
 
 public class DuckEntityRenderer extends GeoEntityRenderer<DuckEntity, DuckEntityRenderState> {
     public DuckEntityRenderer(EntityRendererFactory.Context context) {
@@ -13,10 +12,5 @@ public class DuckEntityRenderer extends GeoEntityRenderer<DuckEntity, DuckEntity
     @Override
     public GeoEntityRenderer<DuckEntity, DuckEntityRenderState> withScale(float scale) {
         return super.withScale(scale);
-    }
-
-    @Override
-    public ChickenEntityRenderState createRenderState() {
-        return new ChickenEntityRenderState();
     }
 }
--- src/main/java/net/tythos/ducksunlimited/entity/DuckEntity.java
+++ src/main/java/net/tythos/ducksunlimited/entity/DuckEntity.java
@@ -12,11 +12,9 @@ import net.minecraft.world.World;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
-import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
-import software.bernie.geckolib.animatable.processing.AnimationController;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class DuckEntity extends AnimalEntity implements GeoEntity {
@@ -33,46 +31,54 @@ public class DuckEntity extends AnimalEntity implements GeoEntity {
                 .add(EntityAttributes.MAX_HEALTH, 4.0D);
     }
 
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<GeoAnimatable>(this, "controller", 10, this::predicate));
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return this.cache;
-    }
-
-    @Override
-    public double getTick(Object object) {
-        return getWorld().getTime();
-    }
-
-    @Override
-    public boolean isBreedingItem(ItemStack stack) {
-        return false; // TODO: Implement proper breeding items
-    }
-
-    @Override
-    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null; // TODO: Implement proper breeding
-    }
+    // @Override
+    // protected void registerGoals() {
+    // this.goalSelector.addGoal(0, new FloatGoal(this));
+    // this.goalSelector.addGoal(1, new PanicGoal(this, 1.4D));
+    // this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+    // this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+    // }
 
     @Override
     public SoundEvent getAmbientSound() {
-        // Will be replaced with duck sound when available
         return SoundEvents.ENTITY_CHICKEN_AMBIENT;
     }
 
     @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_CHICKEN_DEATH;
+    }
+
+    @Override
     protected SoundEvent getHurtSound(DamageSource source) {
-        // Will be replaced with duck sound when available
         return SoundEvents.ENTITY_CHICKEN_HURT;
     }
 
     @Override
-    protected SoundEvent getDeathSound() {
-        // Will be replaced with duck sound when available
-        return SoundEvents.ENTITY_CHICKEN_DEATH;
+    public boolean isBreedingItem(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        // controllers.add(new AnimationController<>(this, "controller", 10,
+        // this::predicate));
+    }
+
+    // private <E extends GeoAnimatable> PlayState predicate(AnimationState<E>
+    // event) {
+    // RawAnimation idle = RawAnimation.begin().thenLoop("idle");
+    // event.getController().setAnimation(idle);
+    // return PlayState.CONTINUE;
+    // }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return this.cache;
     }
 }
