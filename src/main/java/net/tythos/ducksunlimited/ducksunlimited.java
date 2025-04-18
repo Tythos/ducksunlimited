083e158a11dd55e4fe10119d67bd74b7565d09f9
Tythos <bekirkpatrick@proton.me>
2025-04-17 21:56:27 -0700

    wip while fixing

--- src/main/java/net/tythos/ducksunlimited/ducksunlimited.java
+++ src/main/java/net/tythos/ducksunlimited/ducksunlimited.java
@@ -9,24 +9,33 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryKey;
 import net.tythos.ducksunlimited.command.DuckCommands;
+import net.tythos.ducksunlimited.entity.CubeEntity;
 
 public class DucksUnlimited implements ModInitializer {
     public static final String MOD_ID = "ducksunlimited";
 
-    public static final EntityType<DuckEntity> DUCK = Registry.register(
+    // public static final EntityType<DuckEntity> DUCK = Registry.register(
+    // Registries.ENTITY_TYPE,
+    // Identifier.of(MOD_ID, "duck"),
+    // EntityType.Builder.create(DuckEntity::new,
+    // SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build("duck"));
+
+    public static final EntityType<CubeEntity> CUBE = Registry.register(
             Registries.ENTITY_TYPE,
-            Identifier.of(MOD_ID, "duck"),
-            EntityType.Builder.create(DuckEntity::new, SpawnGroup.CREATURE)
-                    .dimensions(0.4f, 0.7f)
-                    .build(RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(MOD_ID, "duck"))));
+            Identifier.of(MOD_ID, "cube"),
+            EntityType.Builder.create(CubeEntity::new, SpawnGroup.CREATURE)
+                    .dimensions(0.75f, 0.75f)
+                    .build(RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(MOD_ID, "cube"))));
 
     @Override
     public void onInitialize() {
-        FabricDefaultAttributeRegistry.register(DUCK, DuckEntity.createDuckAttributes());
-
         CommandRegistrationCallback.EVENT.register(DuckCommands::register);
+
+        FabricDefaultAttributeRegistry.register(CUBE, CubeEntity.createMobAttributes());
     }
 }
---
+++ src/main/java/net/tythos/ducksunlimited/entity/CubeEntity.java
@@ -0,0 +1,11 @@
+package net.tythos.ducksunlimited.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+
+public class CubeEntity extends PathAwareEntity {
+    public CubeEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+}
